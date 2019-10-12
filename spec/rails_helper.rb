d11c48bb00222c8ebc1235c0d00f3874a790dea4
John Paolo <paolo716@gmail.com>
2019-10-12 15:22:49 +0800

    Link articles to Users

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
+include Warden::Test::Helpers
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
