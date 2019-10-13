051c3ae70520a5bf907478c9c1bb087aa93a8352
John Paolo <paolo716@gmail.com>
2019-10-13 11:53:25 +0800

    Show new article button signed in users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -2,10 +2,35 @@ require "rails_helper"
 
 RSpec.feature 'Listing Articles' do
     before do
-        john = User.create(email: "john@example.com", password: "password")
-        @article1 = Article.create(title: "The first article", body: "Lorem ipsum dolar sit omet", user: john)
+        @john = User.create(email: "john@example.com", password: "password")
+        @article1 = Article.create(title: "The first article", body: "Lorem ipsum dolar sit omet", user: @john)
 
-        @article2 = Article.create(title: "The second article", body: "Body of 2nd article", user: john)
+        @article2 = Article.create(title: "The second article", body: "Body of 2nd article", user: @john)
+    end
+
+    scenario "with articles created and user not signed in" do
+        visit "/"
+
+        expect(page).to have_content(@article1.title)
+        expect(page).to have_content(@article1.body) 
+        expect(page).to have_content(@article2.title)
+        expect(page).to have_content(@article2.body)
+        expect(page).to have_link(@article1.title)
+        expect(page).to have_link(@article2.title) 
+        expect(page).not_to have_link("New Article")          
+    end
+
+    scenario "with articles created and user signed in" do
+        login_as(@john)
+        visit "/"
+
+        expect(page).to have_content(@article1.title)
+        expect(page).to have_content(@article1.body) 
+        expect(page).to have_content(@article2.title)
+        expect(page).to have_content(@article2.body)
+        expect(page).to have_link(@article1.title)
+        expect(page).to have_link(@article2.title) 
+        expect(page).to have_link("New Article")          
     end
 
     scenario "A user list all articles" do
@@ -18,6 +43,7 @@ RSpec.feature 'Listing Articles' do
         expect(page).to have_link(@article1.title)
         expect(page).to have_link(@article2.title)          
     end
+
 
     scenario "A user has no articles" do
         Article.delete_all
